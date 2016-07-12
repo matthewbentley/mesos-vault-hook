65d4e7c8ea0837affe1c67cd22bb18ac53d00b0a
Matthew Bentley <bentley@yelp.com>
2016-07-12 14:45:32 -0700

    Rename package

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 itest_trusty: docker_trusty
 	mkdir -p dist/trusty
-	docker run --rm -v $(CURDIR):/build -v $(CURDIR)/dist:/dist mesos-hooks-build-trusty /build/build.sh
+	docker run --rm -v $(CURDIR):/src -v $(CURDIR)/dist:/dist mesos-hooks-build-trusty /src/build.sh
 
 docker_trusty:
 	docker build -t mesos-hooks-build-trusty .
 
 clean:
-	rm -rf dist/* modules
+	rm -rf dist/*
--- build.sh
+++ build.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-VERSION=0.0.1
+VERSION=0.1.0
 EXTRA=yelp1
 
-cd /build/src
+cd /src/src
 ./bootstrap
-mkdir build
-cd build
-../configure --with-mesos=/usr/local/ --prefix=/usr
+mkdir /build
+cd /build
+/src/src/configure --with-mesos=/usr/local/ --prefix=/usr
 make
 mkdir /dest
 make install DESTDIR=/dest
 
-fpm -s dir -t deb --deb-no-default-config-files -n mesos-hooks -v $VERSION --iteration $EXTRA -C /dest -p mesos-hooks_VERSION_ARCH.deb -d "mesos" usr/lib
-cp mesos-hooks*.deb /dist/trusty/
+fpm -s dir -t deb --deb-no-default-config-files -n mesos-vault-hook -v $VERSION --iteration $EXTRA -C /dest -p mesos-vault-hook_VERSION_ARCH.deb -d "mesos" usr/lib
+cp mesos-vault-hook*.deb /dist/trusty/
