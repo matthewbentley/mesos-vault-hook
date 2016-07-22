28229480991b6331302e490156c2ed8066898c1f
Matthew Bentley <bentley@yelp.com>
2016-07-21 19:10:14 -0700

    Info dump

--- src/hook/vault_hook_module.cpp
+++ src/hook/vault_hook_module.cpp
@@ -22,30 +22,12 @@
 
 #include <mesos/module/hook.hpp>
 
-#include <process/protobuf.hpp>
-#include <process/process.hpp>
-#include <process/future.hpp>
-
-#include <stout/none.hpp>
-#include <stout/nothing.hpp>
 #include <stout/result.hpp>
-#include <stout/try.hpp>
-#include <stout/option.hpp>
-#include <stout/foreach.hpp>
-#include <stout/os.hpp>
 
 using namespace mesos;
 
 using std::map;
 using std::string;
-
-using process::Failure;
-using process::Future;
-
-const char* testLabelKey = "MESOS_Test_Label";
-const char* testLabelValue = "ApacheMesos";
-const char* testRemoveLabelKey = "MESOS_Test_Remove_Label";
-const char* testErrorLabelKey = "MESOS_Test_Error_Label";
 
 
 class VaultHook : public Hook
@@ -58,6 +40,7 @@ public:
       const ExecutorInfo& executorInfo)
   {
     LOG(INFO) << "Executing 'slaveExecutorEnvironmentDecorator' hook";
+    LOG(INFO) << "ExecutorID: " << executorInfo.executor_id().value() << ". Name: " << executorInfo.name() << ". CommandInfoValue: " << executorInfo.command().value() << ".";
 
     Environment environment;
 
@@ -70,71 +53,6 @@ public:
     variable->set_value("bar");
 
     return environment;
-  }
-
-
-  // In this hook, look for the existence of a specific label.
-  // If found, return a `Failure`.
-  // Otherwise, add an environment variable to the task.
-  // works: no
-  virtual Future<Option<Environment>> slavePreLaunchDockerEnvironmentDecorator(
-      const Option<TaskInfo>& taskInfo,
-      const ExecutorInfo& executorInfo,
-      const string& name,
-      const string& sandboxDirectory,
-      const string& mappedDirectory,
-      const Option<map<string, string>>& env)
-  {
-    LOG(INFO) << "Executing 'slavePreLaunchDockerEnvironmentDecorator' hook";
-
-    if (taskInfo.isSome()) {
-      foreach (const Label& label, taskInfo->labels().labels()) {
-        if (label.key() == testErrorLabelKey) {
-          return Failure("Spotted error label");
-        }
-      }
-    }
-
-    Environment environment;
-    Environment::Variable* variable = environment.add_variables();
-    variable->set_name("FOO_DOCKER");
-    variable->set_value("docker_bar");
-
-    return environment;
-  }
-
-
-  // works: runs
-  virtual Try<Nothing> slavePreLaunchDockerHook(
-      const ContainerInfo& containerInfo,
-      const CommandInfo& commandInfo,
-      const Option<TaskInfo>& taskInfo,
-      const ExecutorInfo& executorInfo,
-      const string& name,
-      const string& sandboxDirectory,
-      const string& mappedDirectory,
-      const Option<Resources>& resources,
-      const Option<map<string, string>>& env)
-  {
-    LOG(INFO) << "Executing 'slavePreLaunchDockerHook' hook";
-    return os::touch(path::join(sandboxDirectory, "foo"));
-  }
-
-
-  // works: no
-  virtual Try<Nothing> slavePostFetchHook(
-      const ContainerID& containerId,
-      const string& directory)
-  {
-    LOG(INFO) << "Executing 'slavePostFetchHook' hook";
-
-    const string path = path::join(directory, "post_fetch_hook");
-
-    if (os::exists(path)) {
-      return os::rm(path);
-    } else {
-      return Nothing();
-    }
   }
 
 };
